a6bb938d4b90d8d20a5c812b1f11cba66c7df539
voffan <vv.everstov@s-vfu.ru>
2024-11-22 13:21:24 +0900

    token check done

--- backend/sport/users/urls.py
+++ backend/sport/users/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import UserViewSet
+from .views import UserViewSet, CheckToken
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('users/', UserViewSet.as_view({'get': 'list'})),
     path('api-token-auth/', views.obtain_auth_token),
+    path('check-token/', CheckToken.as_view()),
 ]
--- backend/sport/users/views.py
+++ backend/sport/users/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
@@ -11,3 +14,12 @@ class UserViewSet(ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class CheckToken(APIView):
+    queryset = User.objects.all()
+
+    def get(self, request):
+        user = get_object_or_404(Token, key=request.headers['Authorization'].split()[-1]).user
+        result = user is not None
+        return Response({'detail': result})
