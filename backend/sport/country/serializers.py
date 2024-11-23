7edeb6b64b2474defbcd0acaafa77124fcb7f032
voffan <vv.everstov@s-vfu.ru>
2024-11-23 11:02:26 +0900

    Serializers finished

--- backend/sport/country/serializers.py
+++ backend/sport/country/serializers.py
@@ -1,3 +1,26 @@
 from rest_framework import serializers
 from .models import Country, District, Region, City
 
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ['name']
+
+
+class DistrictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = District
+        fields = ['name']
+
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ['district', 'name']
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ['region', 'name']
