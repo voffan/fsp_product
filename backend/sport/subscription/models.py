e195fe65d9a5f5e29f23a129b5c406afca8b8abc
Vladislav Argunov <90658819+kawasaki4112@users.noreply.github.com>
2024-11-23 21:37:37 +0900

    added send notifications

--- backend/sport/subscription/models.py
+++ backend/sport/subscription/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from contests.models import Contest
+
 
 # Create your models here.
 
 class Subscription(models.Model):
+    from contests.models import Contest
     user = models.ForeignKey(User, verbose_name="Пользователь", db_index=True, null=False, on_delete=models.CASCADE)
     contest = models.ForeignKey(Contest, verbose_name="Соревнование", db_index=True, null=False, on_delete=models.CASCADE)
