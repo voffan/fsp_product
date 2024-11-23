c6441fc13c7129853a0379e6b8e30ebd385011cf
Vladislav Argunov <90658819+kawasaki4112@users.noreply.github.com>
2024-11-23 23:13:52 +0900

    subscription delete

--- backend/sport/subscription/urls.py
+++ backend/sport/subscription/urls.py
@@ -5,6 +5,6 @@ from .views import SubscriptionView
 urlpatterns = [
     path('subscriptions/', SubscriptionView.as_view({'get': 'list'})),
     path('subscribe/', SubscriptionView.as_view({'post': 'subscribe'})),
-    path('subscriptions/delete/<int:subscription_id>/', SubscriptionView.as_view(), name='notification-delete'),
+    path('subscriptions/delete/<int:subscription_id>/', SubscriptionView.as_view({'delete': 'delete'})),
 
 ]
